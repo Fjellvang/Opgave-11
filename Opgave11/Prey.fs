96f5322f7f0da60665b942032922c412866ffc37
Aleksander Fjellvang <Aleksander@fjellvang.dk>
2016-01-08 11:10:13 +0100

    Tilføjede noter

--- Opgave11/Prey.fs
+++ Opgave11/Prey.fs
@@ -1,4 +1,6 @@
-﻿module Prey
+﻿// compile i mono ved: fsharpc -a Prey.fs -r Animals.dll
+
+module Prey
 open Animal
 
 type Prey() = class
---
+++ Opgave11/Simulation.fs
@@ -0,0 +1,11 @@
+﻿// Compile i mono ved: fsharpc Simpulation.fs -r Animals.dll -r Prey.dll -r Predator.dll
+
+module Simulation
+open Predator
+open Prey
+open Animal
+
+let x = Predator() :> Animal
+
+printf "%b" (x :? Predator)
+ignore(System.Console.ReadLine())
