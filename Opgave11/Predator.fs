bff86770c413211744b4c4a4a7450ba1a956d8e0
Aleksander Fjellvang <Aleksander@fjellvang.dk>
2016-01-08 11:27:29 +0100

    noter

--- Opgave11/Predator.fs
+++ Opgave11/Predator.fs
@@ -1,7 +1,14 @@
-﻿module Predator
+﻿// Compile Modul i Mono ved: fsharpc -a Predator.fs -r Animals.dll
+
+module Predator
 open Animal
 
 type Predator() = class
     inherit Animal()
     // fixed starve time
+    let mutable starvetime = 5.0
+    member this.starveTime with get() = starvetime
+                           and set(v) = starvetime <- v
 end
+
+
