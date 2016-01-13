2a7fc11c495f8d11b0ca8f64891c28d7b03c4a2c
Aleksander Fjellvang <Aleksander@fjellvang.dk>
2016-01-13 18:04:21 +0100

    FINAL COMMIT... håbert jeg

--- Opgave11/Simulation.fs
+++ Opgave11/Simulation.fs
@@ -14,28 +14,25 @@ Prey.breeding <- int x
 printfn "Please specify Starvetime for Predators: (1-9999)"
 let y = System.Console.ReadLine()
 Predator.starving <- int y
+printfn "Please specify ticks:"
+let z = int(System.Console.ReadLine())
+
+let mutable i = 0
 while running do
     island.DrawMap()
-    island.MovePrey()
-    island.MovePredator()
-    for x in island.PreyArray do
-        island.breedPrey(x)
-        x.Tick()
-    for x in island.PredatorArray do
-        island.breedPredator(x)
-        island.starve(x)
-        island.eat(x)
-        x.Tick()
-    (*for prey in island.PreyArray do
-        try
-            prey.Move(island.getFreeSpot(prey))
-        with
-            | :? System.Exception -> ignore()
-    for predator in island.PredatorArray do
-        try
-            predator.Move(island.getFreeSpot(predator))
-        with
-            | :? System.Exception -> ignore()*)
+    while i < z do
+        island.MovePrey()
+        island.MovePredator()
+        for x in island.PreyArray do
+            island.breedPrey(x)
+            x.Tick()
+        for x in island.PredatorArray do
+            island.breedPredator(x)
+            island.starve(x)
+            island.eat(x)
+            x.Tick()
+        i <- i + 1
+    i <- 0
     let x = System.Console.ReadLine()
     if x = "1" then running <- true
     //System.Console.Clear()
